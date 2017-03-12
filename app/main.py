75a9cfb84572d403c38bb9267d83f2217a8988db
Dexter Sealy <dextersealy@gmail.com>
2017-03-12 10:21:46 -0400

    implement bedrooms/bathrooms filter

--- app/main.py
+++ app/main.py
@@ -26,8 +26,8 @@ def report_memory_usage():
                 result[key] = int(parts[1]) // 1024
     print(', '.join(["{}: {}MB".format(key, value) for key, value in result.items()]))
 
-def find_near(point, distance, fields=None):
-    result = list(mongo_collection.find({'loc' : {'$near' : { '$geometry' : point, '$maxDistance' : distance }}}, fields))
+def find(criteria, fields=None):
+    result = list(mongo_collection.find(criteria, fields))
     if result and '_id' in result[0]:
         for r in result:
             r['_id'] = str(r['_id'])
@@ -51,28 +51,47 @@ def submit():
     #	Retrieve data
 
     data = flask.request.json
-    print(data)
 
     #   Get listings
     
-    distance = data.get('distance', 500)
-    lat, lng = float(data['latitude']), float(data['longitude'])
+    criteria = {}
+    criteria['loc'] = { 
+        '$near' : { 
+            '$geometry' : geojson.Point((float(data['longitude']), float(data['latitude']))), 
+            '$maxDistance' : data.get('distance', 500) 
+            }
+        }
+
+    bedrooms = data.get('bedrooms', '')
+    if bedrooms:
+        bedrooms = int(bedrooms)
+        criteria['bedrooms'] = bedrooms if bedrooms < 4 else { '$gte' : 4 }
+
+    bathrooms = data.get('bathrooms', '')
+    if bathrooms:
+        bathrooms = float(bathrooms)
+        criteria['bathrooms'] = bathrooms if bathrooms < 3.0 else { '$gte' : 3.0 }
+
+    print(criteria)
+
     fields = data.get('fields', ['id_', 'loc', 'price'])
-    listings = find_near(geojson.Point((lng, lat)), distance, fields)
+    listings = find(criteria, fields)
     listings = sorted([l for l in listings if l['price'] < 10000], key=lambda l: l['price'])
 
     #   Get Probability Distribution
 
     prices = [l['price'] for l in listings]
-    pdf = stats.norm.pdf(prices, np.mean(prices), np.std(prices))
-    for i, l in enumerate(listings):
-        l['pdf'] = pdf[i]
+    if prices:
+        pdf = stats.norm.pdf(prices, np.mean(prices), np.std(prices))
+        for i, l in enumerate(listings):
+            l['pdf'] = pdf[i]
+        del pdf
 
     #	Package and return result
     
     print('{} results'.format(len(listings)))
     result = flask.jsonify(listings)
-    del listings, prices, pdf
+    del listings, prices
 
     report_memory_usage()
     return result
---
+++ app/mapstyle.js
@@ -0,0 +1,27 @@
+var map_style = [
+  {
+    "featureType": "poi",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "stylers": [
+      {
+        "visibility": "on"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels.text",
+    "stylers": [
+      {
+        "visibility": "off"
+      }
+    ]
+  }
+]
